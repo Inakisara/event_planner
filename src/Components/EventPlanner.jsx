858e28f8c14492edb6a07d8d7fef9835adb34647
inakisara <inkiaki.saragmail.com>
2024-09-13 11:03:48 -0400

    Corrected my error icorporing the footer into main file

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './EventPlanner.css'; // Import CSS file for styling
 import Footer from './Footer';
-// include Footer component as attribute as below
-<Footer/>
 
 const EventPlanner = () => {
     return (
