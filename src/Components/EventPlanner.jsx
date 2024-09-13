2a65c1f5a59e51306506920075712f9f8188c675
inakisara <inkiaki.saragmail.com>
2024-09-13 11:07:37 -0400

    Incorpored Footer into the body of the page

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './EventPlanner.css'; // Import CSS file for styling
-import Footer form './Footer';
+import Footer from './Footer';
+
 
 const EventPlanner = () => {
     return (
@@ -75,6 +76,7 @@ const EventPlanner = () => {
                     <button className='submit-button'>Send</button>
                 </form>
             </section>
+            <Footer/>
         </div>
     );
 };
